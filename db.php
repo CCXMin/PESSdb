09d28b9dcee60ac200d84033b482d19d936001eb
CCXMin <105761409+CCXMin@users.noreply.github.com>
2022-05-19 10:50:51 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php // define database connection variables are constant
+	define('DB_USER', "project"); //db user
+	define('DB_PASSWORD', "test1234"); //db password
+	define('DB_DATABASE', "pessdb"); //database name
+	define('DB_SERVER', "localhost"); //db server
+?>
